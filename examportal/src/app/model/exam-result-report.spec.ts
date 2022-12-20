6f3a475f7656bb14a7c1a689e741d8ea611e5c32
amol <amol1629rathod@gmail.com>
2022-12-20 11:54:12 +0530

    Revert "first commit"
    This reverts commit cf614692b3eccca91a0a23f984f8129148ccab7d.

--- examportal/src/app/model/exam-result-report.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ExamResultReport } from './exam-result-report';
-
-describe('ExamResultReport', () => {
-  it('should create an instance', () => {
-    expect(new ExamResultReport()).toBeTruthy();
-  });
-});
--- examportal/src/app/model/examination.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Examination } from './examination';
-
-describe('Examination', () => {
-  it('should create an instance', () => {
-    expect(new Examination()).toBeTruthy();
-  });
-});
